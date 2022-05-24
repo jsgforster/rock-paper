069a0ca5fb7f73eb130af572c0e17e9c1cea5f2e
Joshua Forster <jsg.forster@googlemail.com>
2022-05-24 23:28:05 +0700

    Add final score screen and refresh

--- script.js
+++ script.js
@@ -13,77 +13,104 @@ function computerPlay() {
 }
 
 function playRound(e){
+    removeListeners();
     computerSelection = computerPlay();
     playerSelection = this.id
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Tis a draw!`);       
+        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Tis a draw!`,30); 
+        updateScore('draw')      
     } else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors'){
-        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Player Wins!`)
+        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Player Wins!`,30)
+        setTimeout(function(){updateScore('player')},2000)
     } else if (playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock'){
-        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Player Wins!`)
+        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Player Wins!`,30)
+        setTimeout(function(){updateScore('player')},2000)
     } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper'){
-        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Player Wins!`)
+        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Player Wins!`,30)
+        setTimeout(function(){updateScore('player')},2000)
     } else {
-        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Computer Wins!`)
+        typeWrite(`Player has chosen: ${this.id}`,`Computer has chosen: ${computerSelection}`,`Computer Wins!`,30)
+        setTimeout(function(){updateScore('computer')},2000)
     }
 
 }
 
-function check(player){
-    if(player.toLowerCase() == 'rock' || player.toLowerCase() == 'paper' || player.toLowerCase() == 'scissors'){
-        return player
-    } else {
-                console.log('invalid input, try again')
-                let player = prompt('Rock, Paper or Scissors?')
-                check(player)
-                return player
-            }
-    }
+function updateScore(winner){
 
-function game(){
-    let pCount = 0
-    let cCount = 0
-    for (i = 0; i < 5; i++){
-        let comp = computerPlay()
-        let player = prompt('Rock, Paper or Scissors?')
-        player = check(player)
-
-        let outcome = playRound(player,comp)
-        console.log(outcome)
-        if(outcome == 'Player wins'){
-            pCount ++
-            console.log('pcount is ' + pCount)
-            console.log('ccount is ' + cCount)
-        } else if (outcome == 'Computer wins'){
-            cCount ++
-            console.log('pcount is ' + pCount)
-            console.log('ccount is ' + cCount)
+    let pscore = document.getElementById('playerScore').innerHTML;
+    let cscore = document.getElementById('computerScore').innerHTML;
+    if(winner == 'player'){
+        document.getElementById('playerScore').innerHTML++;
+        pscore++;
+        if(pscore == 5){
+            setTimeout(removeOptions,800);
+            setTimeout(function(){typeWrite(`Congratulations! Player has WON!`,`FINAL SCORE`, `Computer:${cscore}        Player:${pscore}`,50)},800);
+            let play = document.getElementById('but');
+            setTimeout(function(){play.style.display = 'block';clearScores()},5000);
         }
-
+    }else if(winner == 'computer'){
+        document.getElementById('computerScore').innerHTML++;
+        cscore++;
+        if(cscore == 5){
+            setTimeout(removeOptions,800);
+            setTimeout(function(){typeWrite(`Oops! Computer has WON!`,`FINAL SCORE`, `Computer:${cscore}        Player:${pscore}`,50)},800);
+            let play = document.getElementById('but');
+            setTimeout(function(){play.style.display = 'block';clearScores()},5000);
+            console.log('removedcompiuter?')
+        }
     }
-    if (pCount < cCount){
-        console.log('Computer wins ' + cCount + ' to ' + pCount +' games.')
-    }else if (cCount < pCount){
-        console.log('Player wins ' + pCount + ' to ' + cCount +' games.')
-    }else {
-        console.log('Tis a draw, both player won ' + cCount + ' games.')
-    }
-    return 'done'
+    
 }
 
 
 let play = document.getElementById('but');
 play.addEventListener('click', function(){
-    typeWrite('Choose your weapon player!','',' ');document.getElementById("but").remove();
+    typeWrite('Choose your weapon player!','',' ',50);
+    document.getElementById("but").style.display = 'none';
+    addOptions();
+    setTimeout(addListeners, 1500);
+    
 });
 
+function addListeners(){
+    const options = document.querySelectorAll('.option');
+    for(i = 0; i < options.length; i++){
+        options[i].addEventListener('click',playRound);
+    }
+}
+
+function removeListeners(){
+    const options = document.querySelectorAll('.option');
+    for(i = 0; i < options.length; i++){
+        console.log(options[i])
+        options[i].removeEventListener('click',playRound);
+    }
+}
+function addOptions(){
+    const options = document.querySelectorAll('.bubbly-button');
+    for(i = 0; i < options.length; i++){
+        console.log(options[i])
+        options[i].style.display = 'block';
+    }
+}
+function removeOptions(){
+    const options = document.querySelectorAll('.bubbly-button');
+    for(i = 0; i < options.length; i++){
+        console.log(options[i])
+        options[i].style.display = 'none';
+    }
+}
+
+function clearScores(){
+    document.getElementById('playerScore').innerHTML = 0;
+    document.getElementById('computerScore').innerHTML = 0;
+    document.getElementById("text1").innerHTML = '';
+    document.getElementById("text2").innerHTML = '';
+    document.getElementById("outcome").innerHTML = '';
+}
 
 
-
-
-function typeWrite(txt1,txt2,outcome){
-    const options = document.querySelectorAll('.option')
-    options.forEach(option => option.removeEventListener('click',playRound));
+function typeWrite(txt1,txt2,outcome,speed){
     document.getElementById("text1").innerHTML = '';
     document.getElementById("text2").innerHTML = '';
     document.getElementById("outcome").innerHTML = '';
@@ -95,17 +122,16 @@ function typeWrite(txt1,txt2,outcome){
     if (i < txt1.length) {
         document.getElementById("text1").innerHTML += txt1.charAt(i);
         i++;
-        setTimeout(typeWriter, 50);
+        setTimeout(typeWriter, speed);
     }else if(txt2 && j < txt2.length){
         document.getElementById("text2").innerHTML += txt2.charAt(j);
         j++;
-        setTimeout(typeWriter, 50);
+        setTimeout(typeWriter, speed);
     }else if(outcome && k < outcome.length){
         document.getElementById("outcome").innerHTML += outcome.charAt(k);
         k++;
-        setTimeout(typeWriter, 50);
-        console.log('run')
-        options.forEach(option => option.addEventListener('click',playRound));
+        setTimeout(typeWriter, speed);
+        setTimeout(addListeners, 1500);
     }
     }
     
